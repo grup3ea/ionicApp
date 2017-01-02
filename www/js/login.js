97806771eead297af3142e35cc9b084e1bd060ad
arnaucode <idoctnef@openmailbox.org>
2017-01-02 12:38:00 +0100

    implementat lu q tenim a la web, de al fer el login enviar el userAgent del device, i la ip --> la ip falla, a la web ho tenim fent un getjson amb jquery, però a la app no tenim jquery, i amb angular està retornant error al format del json que reb

--- www/js/login.js
+++ www/js/login.js
@@ -1,9 +1,9 @@
 var urlapi="http://localhost:3005/api/";
 //var urlapi="http://147.83.7.158:3005/api/";
 
-angular.module('app.globalCtrl', [])
+angular.module('app.globalCtrl', ['ng.deviceDetector'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $window, $ionicLoading) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $window, $ionicLoading, deviceDetector) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,47 +11,59 @@ angular.module('app.globalCtrl', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
+  var vm = this;
+  vm.data = deviceDetector;
+  var ip;
+  //$http({method: 'JSONP', url: '//freegeoip.net/json/?callback'})
+  $http.jsonp('//freegeoip.net/json/?callback=?', {jsonpCallbackParam: 'callback'})
+  .success(function(data, success) {
+      ip = JSON.stringify(data);
+      console.log(ip);
+  })
+  .error(function(data, success){
+    console.log("error getting json of ip info");
+    console.log(data);
+    console.log(success);
+  });
+
+  console.log("userAgent: " + vm.data.raw.userAgent);
   $scope.loginData = {};
-  $scope.signupData= {};
-
-  $scope.storageusername=localStorage.getItem("fs_app_userdata");
-
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    $http({
-        url: urlapi + $scope.loginData.role+ 's/login',
-        method: "POST",
-        data: $scope.loginData
-    })
-    .then(function(response) {
-            // success
-            console.log("response: ");
-            console.log(response.data);
-            if(response.data.success==true)
-            {
-                console.log("login successful. Response.data: ");
-                console.log(response.data);
-                if (response.data.success == true) {
-                    localStorage.setItem("fs_app_token", response.data.token);
-                    localStorage.setItem("fs_app_userdata", JSON.stringify(response.data.user));
-                    window.location.reload();
-                }
-            }else{
-                console.log("login failed");
-                $ionicLoading.show({ template: 'Login failed, user or password error.', noBackdrop: true, duration: 2000 });
-            }
-
-
+  $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+      $scope.loginData.userAgent=vm.data.raw.userAgent;//aquí li afegin el userAgent al post del loginData
+      $scope.loginData.os=vm.data.os;
+      $scope.loginData.browser=vm.data.browser;
+      $scope.loginData.device=vm.data.device;
+      $scope.loginData.os_version=vm.data.os_version;
+      $scope.loginData.ip = ip;
+      $scope.loginData.browser_version=vm.data.browser_version;
+      $http({
+          url: urlapi + $scope.loginData.role+ 's/login',
+          method: "POST",
+          data: $scope.loginData
+      })
+      .then(function (response) {
+      // success
+      console.log("response: ");
+      console.log(response.data);
+      if (response.data.success == true) {
+        console.log("login successful. Response.data: ");
+              console.log(response.data);
+              if (response.data.success == true)
+              {
+                  localStorage.setItem("fs_app_token", response.data.token);
+                  localStorage.setItem("fs_app_userdata", JSON.stringify(response.data.user));
+                  window.location.reload();
+              }
+      } else {
+        console.log("login failed");
+        $ionicLoading.show({ template: 'Login failed, user or password error.', noBackdrop: true, duration: 2000 });
+      }
     },
-    function(response) { // optional
-            // failed
-            console.log(response);
+    function (response) { // optional
+        // failed
+        console.log(response);
     });
-
   };
   $scope.doSignup = function() {
     $scope.signupData.role="client";
