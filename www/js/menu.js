68d4bc7a9b9d4bf17568586063dd07bf46b4d00d
arnaucode <idoctnef@openmailbox.org>
2017-01-11 13:15:53 +0100

    actualitzant l'app, fet side menu, i pagina main

--- www/js/menu.js
+++ www/js/menu.js
@@ -8,4 +8,50 @@ angular.module('app.menu', [])
       localStorage.removeItem("fs_app_userdata");
       window.location = "/";
   };
+
+  var sidenavImg="img/sidenav/";
+  $scope.options = [
+      {
+          title: "Main",
+          description: "description",
+          link: "#/app/main",
+          icon: sidenavImg + "dashboard.png"
+      },
+      {
+          title: "Dashboard",
+          description: "description",
+          link: "#/app/dashboard",
+          icon: sidenavImg + "dashboard.png"
+      },
+      {
+          title: "Notifications",
+          description: "description",
+          link: "#/app/notifications",
+          icon: "img/notification.png"
+      },
+      {
+          title: "Network",
+          description: "description",
+          link: "#/app/network",
+          icon: sidenavImg + "network.png"
+      },
+      {
+          title: "Training",
+          description: "description",
+          link: "#/app/training",
+          icon: sidenavImg + "training.png"
+      },
+      {
+          title: "Diet",
+          description: "description",
+          link: "#/app/eating",
+          icon: sidenavImg + "diet.png"
+      },
+      {
+          title: "Settings",
+          description: "description",
+          link: "#/app/settings",
+          icon: sidenavImg + "settings.png"
+      }
+  ];
 });
