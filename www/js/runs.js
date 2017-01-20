e2262fd3a08fb5dc5453a65ec38fbbe5e467cab6
arnaucode <idoctnef@openmailbox.org>
2017-01-20 09:38:44 +0100

    improved

--- www/js/runs.js
+++ www/js/runs.js
@@ -4,14 +4,18 @@ angular.module('app.runs', [])
 
   $scope.storageuser=JSON.parse(localStorage.getItem("fs_app_userdata"));
   $scope.user=[];
-  $http.get(urlapi + "/runs/byUserId/" + $scope.storageuser._id)
-    .then(function (data) {
-        console.log('data success');
-        console.log(data); // for browser console
-        $scope.user=data.data;
-    }, function (data, status) {
-        console.log('data error');
-        console.log(status);
-        console.log(data);
-    });
+  $scope.doRefresh=function(){
+      $http.get(urlapi + "/runs/byUserId/" + $scope.storageuser._id)
+        .then(function (data) {
+            console.log('data success');
+            console.log(data); // for browser console
+            $scope.user=data.data;
+            $scope.$broadcast('scroll.refreshComplete');//refresher stop
+        }, function (data, status) {
+            console.log('data error');
+            console.log(status);
+            console.log(data);
+        });
+    };
+    $scope.doRefresh();
 });
